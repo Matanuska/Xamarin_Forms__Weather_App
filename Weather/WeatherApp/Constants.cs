845e4008d6e340864c3de593b9f6716b965a637a
Denis BOUVET <denis.bouvet@live.com>
2020-04-25 09:04:13 +0200

    Ajoutez des fichiers projet.

---
+++ Weather/WeatherApp/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "f90816cb19833ee5abb1c6e9e8d3084f";
+    }
+}
